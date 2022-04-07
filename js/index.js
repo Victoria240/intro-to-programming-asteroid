88f973577d41a22496546ee935894192e9133fbb
Victoria240 <taiwovictoria024@gmail.com>
2022-04-07 13:09:04 +0100

    Add Footer Element using DOM Manipulation

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var today = new Date("March 28, 2022 11:35:00");
+var thisYear = today.getFullYear();
+let footer = document.querySelector('footer');
+let copyright = document.createElement('p');
+document.copyright.innerText = 'thisYear';
+footer.append(copyright)
