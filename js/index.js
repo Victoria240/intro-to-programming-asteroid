8d0a51f1d0e9dc0ea513ef57e4fd580224c8eb33
Victoria240 <taiwovictoria024@gmail.com>
2022-04-07 13:29:18 +0100

    made modifications to Javascript files

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-var today = new Date("March 28, 2022 11:35:00");
+var today = new Date();
 var thisYear = today.getFullYear();
+document.getElementById("copyright").innerHTML = thisYear
 let footer = document.querySelector('footer');
 let copyright = document.createElement('p');
-document.copyright.innerText = 'thisYear';
+document.copyright.innerText = thisYear;
 footer.append(copyright)
