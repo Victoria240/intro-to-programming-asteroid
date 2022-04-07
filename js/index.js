6781c472f2be05b74821e16083025e78db7f2de1
Victoria240 <taiwovictoria024@gmail.com>
2022-04-07 13:33:37 +0100

    added year to copyright

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var today = new Date();
 var thisYear = today.getFullYear();
-document.getElementById("copyright").innerHTML = thisYear
-let footer = document.querySelector('footer');
-let copyright = document.createElement('p');
-document.copyright.innerText = thisYear;
-footer.append(copyright)
+document.getElementById("currentYear").innerHTML = thisYear;
+// let footer = document.querySelector('footer');
+// let copyright = document.createElement('p');
+// document.copyright.innerText = thisYear;
+// footer.append(copyright)
