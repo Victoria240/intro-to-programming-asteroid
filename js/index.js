ea2d9be51f9fd8c62f07b16c4bf8388f123063df
Victoria240 <taiwovictoria240@gmail.com>
2023-03-17 14:15:03 +0000

    New commit

--- js/index.js
+++ js/index.js
@@ -19,14 +19,15 @@ for (let i=0; i<skills.length; i++) {
     let messageForm = document.getElementsByName('leave_message');  
     console.log (messageForm)
  
-    messageForm.addEventListener("submit", (event) =>{
+    messageForm[0].addEventListener("submit", (event) =>{
         event.preventDefault()
         const name = event.target.name
         const email = event.target.email
         const message = event.target.message
         console.log(name.value, email.value, message.value );
         const messageSection = document.getElementById("messages");
-        const messageList = messageSection.querySelector('ul')
+        console.log (messageSection)
+        const messageList = messageSection.querySelector('ul') 
         const newMessage = document.createElement('li')
         newMessage.innerHTML=`<a href="mailto:${email.value}" target="_blank"> ${name.value}</a>
         <span> wrote a message: ${message.value}</span>`
@@ -39,6 +40,6 @@ for (let i=0; i<skills.length; i++) {
         })
         newMessage.appendChild(removeButton)
         messageList.appendChild(newMessage)
-        messageForm.reset()
+        messageForm[0].reset()
         
     })
